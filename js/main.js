0eaf7d74766ba554e5efc04a2441ae1f00522a11
jw-lab jw <jdb0731@naver.com>
2023-05-22 06:40:04 +0000

    update slider

--- js/main.js
+++ js/main.js
@@ -1,12 +1,27 @@
 window.addEventListener("load",() => {
-    //p : portfolio 종류 
+    //슬라이드 버튼
+    const prevBtns = document.querySelectorAll(".slide-prev-button");
+    const nextBtns = document.querySelectorAll(".slide-next-button");
+    prevBtns.forEach((e)=>{
+        e.addEventListener("click", () => {
+            const btnKindId = e.parentNode.id;
+            const btnKind = btnKindId.slice(-1);
+            slide(btnKind, "prev");
+        });
+    })
+    nextBtns.forEach((e)=>{
+        e.addEventListener("click", () => {
+            const btnKindId = e.parentNode.id;
+            const btnKind = btnKindId.slice(-1);
+            slide(btnKind, "next");
+        });
+    })
+    //현재 portfolio slide 
     let currSlide = 1;
+    //p : portfolio 종류 , btn : prev, next
     const slide = (p, btn) => {
         const slider = document.querySelector(`#portfolioModal${p} .slider`);
         let slideWidth = slider.clientWidth;
-
-        // const prevBtn = document.querySelector(`#portfolioModal${p} .slide_prev_button`);
-        // const nextBtn = document.querySelector(`#portfolioModal${p} .slide_next_button`);
 
         const slideItems = document.querySelectorAll(`#portfolioModal${p} .slider li`);
         const maxSlide = slideItems.length;
@@ -34,76 +49,25 @@ window.addEventListener("load",() => {
                 currSlide++;
             }
         }
-        // nextBtn.addEventListener("click", () => {
-        // currSlide++;
-        // // 마지막 슬라이드 이상으로 넘어가지 않게 하기 위해서
-        // if (currSlide <= maxSlide) {
-        //     const offset = slideWidth * (currSlide - 1);
-        //     slideItems.forEach((i) => {
-        //     i.setAttribute("style", `left: ${-offset}px`);
-        //     });
-        // } else {
-        //     currSlide--;
-        // }
-        // });
-
-        // prevBtn.addEventListener("click", () => {
-        // currSlide--;
-        // // 1번째 슬라이드 이하로 넘어가지 않게 하기 위해서
-        // if (currSlide > 0) {
-        //     const offset = slideWidth * (currSlide - 1);
-        //     slideItems.forEach((i) => {
-        //     i.setAttribute("style", `left: ${-offset}px`);
-        //     });
-        // } else {
-        //     currSlide++;
-        // }
-        // });
         // 브라우저 화면이 조정될 때 마다 slideWidth를 변경하기 위해
         window.addEventListener("resize", () => {
             slideWidth = slide.clientWidth;
         });
     };
-
-    const prevBtn = document.querySelector(".slide_prev_button");
-    const nextBtn = document.querySelector(".slide_next_button");
-
-    nextBtn.addEventListener("click", () => {
-        slide(1, "next");
-    });
-
-    prevBtn.addEventListener("click", () => {
-        slide(1, "prev");
-    });
-    
-  
-
-    // const slide = (p,e)=>{
-    //     const slider = document.querySelector(`#section${p} .slider`);
-    //     const sliderLis = slider.querySelectorAll("li");
-    //     const liWidth = sliderLis[0].clientWidth; //1개 width
-    //     const lisWidth = liWidth * sliderLis.length
-    //     slider.style.width = `${lisWidth}px`;
-
-    //     let currentIdx = sliderIdx[`page${p}`];
-    //     let xpos = sliderXpos[`page${p}`];
-    //     if (e.deltaY > 0) {
-    //         if (currentIdx < sliderLis.length - 1) {
-    //             xpos -= liWidth;
-    //             slider.style.transform = `translateX(${xpos}px)`;
-    //             currentIdx++;
-    //         } 
-    //     } else if(e.deltaY < 0) {
-    //         if (currentIdx > 0) {
-    //             xpos += liWidth;
-    //             slider.style.transform = `translateX(${xpos}px)`;
-    //             currentIdx--;
-    //         } 
-    //     }
-    //     sliderIdx[`page${p}`] = currentIdx;
-    //     sliderXpos[`page${p}`] = xpos;
-
-    //     return;
-    // }
-
+        
+    //portfolio 클릭 시 초기화
+    const portfolioItems = document.querySelectorAll(".portfolio-item");
+    portfolioItems.forEach((e)=>{
+        e.addEventListener("click", () => {
+            //현재 슬라이드 초기화
+            currSlide = 1;
+            //각 모달 슬라이드 offset 0으로 리셋
+            const slideItems = document.querySelectorAll(`.slider li`);
+            const offset = 0;
+                slideItems.forEach((i) => {
+                i.setAttribute("style", `left: ${-offset}px`);
+            });
+            
+        });
+    })
 });
