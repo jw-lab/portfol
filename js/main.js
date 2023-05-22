2fe78370c7eec5cee636d6cc2161cf804913b508
jw-lab jw <jdb0731@naver.com>
2023-05-22 03:04:13 +0000

    add slider

--- js/main.js
+++ js/main.js
@@ -1,115 +1,109 @@
 window.addEventListener("load",() => {
-    // const vHtml = $("html");
-    // //onload시 position top 0 으로 
-    // vHtml.animate({scrollTop : 0},10);
-    
-    // //mouse wheel event start
-    // document.addEventListener("wheel", e => {
-    //     e.preventDefault();
-    //     //animate 실행 중이면 return
-    //     if(vHtml.is(":animated")) return;
+    //p : portfolio 종류 
+    let currSlide = 1;
+    const slide = (p, btn) => {
+        const slider = document.querySelector(`#portfolioModal${p} .slider`);
+        let slideWidth = slider.clientWidth;
 
-    //     // get Y-axis value of each div:
-    //     var div1y = $('#section1').offset().top,
-    //         div2y = $('#section2').offset().top,
-    //         div3y = $('#section3').offset().top,
-    //         div4y = $('#section4').offset().top,
-    //         div5y = $('#section5').offset().top,
-    //         // get window's current scroll position:
-    //         lastScrollTop = $(this).scrollTop(),
-    //         // for getting user's scroll direction:
-    //         scrollDirection,
-    //         // for determining the previous and next divs to scroll to, based on lastScrollTop:
-    //         targetUp,
-    //         targetDown,
-    //         // for determining which of targetUp or targetDown to scroll to, based on scrollDirection:
-    //         targetElement;
+        // const prevBtn = document.querySelector(`#portfolioModal${p} .slide_prev_button`);
+        // const nextBtn = document.querySelector(`#portfolioModal${p} .slide_next_button`);
 
-    //         // get scroll direction:
-    //         if ( e.deltaY > 0 ) {
-    //         scrollDirection = 'up';
-    //         } else if ( e.deltaY <= 0 ) {
-    //         scrollDirection = 'down';
-    //         } 
-
-    //     // condition: determine the previous and next divs to scroll to, based on lastScrollTop:
-    //     if (lastScrollTop === div1y) {
-    //     targetUp = $('#section1');
-    //     targetDown = $('#section2');
-    //     } else if (lastScrollTop === div2y) {
-    //     targetUp = $('#section1');
-    //     targetDown = $('#section3');
-    //     } else if (lastScrollTop === div3y) {
-    //     targetUp = $('#section2');
-    //     targetDown = $('#section4');
-    //     } else if (lastScrollTop === div4y) {
-    //     targetUp = $('#section3');
-    //     targetDown = $('#section5');
-    //     } else if (lastScrollTop === div5y) {
-    //     targetUp = $('#section4');
-    //     targetDown = $('#section6');
-    //     } else if (lastScrollTop === div6y) {
-    //     targetUp = $('#section5');
-    //     targetDown = $('#section6');
-    //     } else if (lastScrollTop < div2y) {
-    //     targetUp = $('#section1');
-    //     targetDown = $('#section2');
-    //     } else if (lastScrollTop < div3y) {
-    //     targetUp = $('#section2');
-    //     targetDown = $('#section3');
-    //     } else if (lastScrollTop < div4y) {
-    //     targetUp = $('#section3');
-    //     targetDown = $('#section4');
-    //     } else if (lastScrollTop < div5y) {
-    //     targetUp = $('#section4');
-    //     targetDown = $('#section5');
-    //     } else if (lastScrollTop < div6y) {
-    //     targetUp = $('#section5');
-    //     targetDown = $('#section6');
-    //     } else if (lastScrollTop > div6y) {
-    //     targetUp = $('#section6');
-    //     targetDown = $('#section6');
-    //     } // end else if
-
-    //     // condition: determine which of targetUp or targetDown to scroll to, based on scrollDirection:
-    //     if (scrollDirection === 'down') {
-    //     targetElement = targetDown;
-    //     } else if (scrollDirection === 'up') {
-    //     targetElement = targetUp;
-    //     } // end else if
-
-    //     const posTop =(page-1) * window.innerHeight;
-    //     vHtml.animate({scrollTop : posTop});
-
-    // }, {passive:false})
-
-
-    const slide = (p,e)=>{
-        const slider = document.querySelector(`#section${p} .slider`);
-        const sliderLis = slider.querySelectorAll("li");
-        const liWidth = sliderLis[0].clientWidth; //1개 width
-        const lisWidth = liWidth * sliderLis.length
-        slider.style.width = `${lisWidth}px`;
-
-        let currentIdx = sliderIdx[`page${p}`];
-        let xpos = sliderXpos[`page${p}`];
-        if (e.deltaY > 0) {
-            if (currentIdx < sliderLis.length - 1) {
-                xpos -= liWidth;
-                slider.style.transform = `translateX(${xpos}px)`;
-                currentIdx++;
-            } 
-        } else if(e.deltaY < 0) {
-            if (currentIdx > 0) {
-                xpos += liWidth;
-                slider.style.transform = `translateX(${xpos}px)`;
-                currentIdx--;
-            } 
+        const slideItems = document.querySelectorAll(`#portfolioModal${p} .slider li`);
+        const maxSlide = slideItems.length;
+        
+        if (btn == "next") {
+            currSlide++;
+            // 마지막 슬라이드 이상으로 넘어가지 않게 하기 위해서
+            if (currSlide <= maxSlide) {
+                const offset = slideWidth * (currSlide - 1);
+                slideItems.forEach((i) => {
+                i.setAttribute("style", `left: ${-offset}px`);
+                });
+            } else {
+                currSlide--;
+            }  
+        } else if(btn == "prev"){
+            currSlide--;
+            // 1번째 슬라이드 이하로 넘어가지 않게 하기 위해서
+            if (currSlide > 0) {
+                const offset = slideWidth * (currSlide - 1);
+                slideItems.forEach((i) => {
+                i.setAttribute("style", `left: ${-offset}px`);
+                });
+            } else {
+                currSlide++;
+            }
         }
-        sliderIdx[`page${p}`] = currentIdx;
-        sliderXpos[`page${p}`] = xpos;
+        // nextBtn.addEventListener("click", () => {
+        // currSlide++;
+        // // 마지막 슬라이드 이상으로 넘어가지 않게 하기 위해서
+        // if (currSlide <= maxSlide) {
+        //     const offset = slideWidth * (currSlide - 1);
+        //     slideItems.forEach((i) => {
+        //     i.setAttribute("style", `left: ${-offset}px`);
+        //     });
+        // } else {
+        //     currSlide--;
+        // }
+        // });
 
-        return;
-    }
+        // prevBtn.addEventListener("click", () => {
+        // currSlide--;
+        // // 1번째 슬라이드 이하로 넘어가지 않게 하기 위해서
+        // if (currSlide > 0) {
+        //     const offset = slideWidth * (currSlide - 1);
+        //     slideItems.forEach((i) => {
+        //     i.setAttribute("style", `left: ${-offset}px`);
+        //     });
+        // } else {
+        //     currSlide++;
+        // }
+        // });
+        // 브라우저 화면이 조정될 때 마다 slideWidth를 변경하기 위해
+        window.addEventListener("resize", () => {
+            slideWidth = slide.clientWidth;
+        });
+    };
+
+    const prevBtn = document.querySelector(".slide_prev_button");
+    const nextBtn = document.querySelector(".slide_next_button");
+
+    nextBtn.addEventListener("click", () => {
+        slide(1, "next");
+    });
+
+    prevBtn.addEventListener("click", () => {
+        slide(1, "prev");
+    });
+    
+  
+
+    // const slide = (p,e)=>{
+    //     const slider = document.querySelector(`#section${p} .slider`);
+    //     const sliderLis = slider.querySelectorAll("li");
+    //     const liWidth = sliderLis[0].clientWidth; //1개 width
+    //     const lisWidth = liWidth * sliderLis.length
+    //     slider.style.width = `${lisWidth}px`;
+
+    //     let currentIdx = sliderIdx[`page${p}`];
+    //     let xpos = sliderXpos[`page${p}`];
+    //     if (e.deltaY > 0) {
+    //         if (currentIdx < sliderLis.length - 1) {
+    //             xpos -= liWidth;
+    //             slider.style.transform = `translateX(${xpos}px)`;
+    //             currentIdx++;
+    //         } 
+    //     } else if(e.deltaY < 0) {
+    //         if (currentIdx > 0) {
+    //             xpos += liWidth;
+    //             slider.style.transform = `translateX(${xpos}px)`;
+    //             currentIdx--;
+    //         } 
+    //     }
+    //     sliderIdx[`page${p}`] = currentIdx;
+    //     sliderXpos[`page${p}`] = xpos;
+
+    //     return;
+    // }
 
 });
